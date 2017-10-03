852e1e619e9c7384691a7067af94d60bd0985316
Taylor Collins <taylor@tcdihq.com>
2017-10-02 23:16:35 -0400

    Add support for UTM URL parameters (append them to the "source" URL)

--- src/Anakadote/BAMLT/BAMLT.php
+++ src/Anakadote/BAMLT/BAMLT.php
@@ -5,7 +5,7 @@ namespace Anakadote\BAMLT;
 /**
  * Send a lead to the default BAM Lead Tracker web service.
  *
- * @version  1.0.0
+ * @version  1.1.0
  * @author   Taylor Collins <taylor@tcdihq.com>
  */
 class BAMLT
@@ -17,7 +17,7 @@ class BAMLT
      */
     private $allowed_inputs = [
         'first_name', 'last_name', 'email', 'phone', 'phone_ext', 'address', 'address_2', 'city', 'state', 'zip', 'interest', 'comments', 
-        'lead_generator', 'delivery_source', 'media_type', 'referrer_token',
+        'lead_generator', 'delivery_source', 'media_type', 'referrer_token', 'utm',
     ];
     
     /**
@@ -47,46 +47,52 @@ class BAMLT
      */
     public function send($uri, $input, $is_client_uri = false)
     {        
-        if(! $uri) return false;
-        if(! is_array($input)) return false;
+        if (! $uri) return false;
+        if (! is_array($input)) return false;
         
         // Allow a "name" input
-        if(isset($input['name'])){
+        if (isset($input['name'])) {
             $name = explode(' ', preg_replace('/\s+/', ' ', (trim($input['name']))));
             $input['first_name'] = isset($name[0]) ? $name[0] : '';
             $input['last_name']  = isset($name[1]) ? $name[1] : '';
         }
         
         // Loop through supplied data and take allowed values
-        foreach($input as $key => $value){
-            if(in_array($key, $this->allowed_inputs)){
+        foreach ($input as $key => $value) {
+            if (in_array($key, $this->allowed_inputs)) {
                 $this->setData($key, $value);
             }
         }
-                
+        
+        // Build the source url
+        $source = $_SERVER['HTTP_REFERER'];
+        if (! empty($input['utm'])) {
+            $source .= '?' . http_build_query($input['utm']);
+        }
+        
         // The XML
         $xml  = "<?xml version='1.0'?><root>";
-        $xml .= "<source>" . $this->cleanForXML($_SERVER['HTTP_REFERER']) . "</source>";
+        $xml .= "<source>" . $this->cleanForXML($source) . "</source>";
         
-        if($is_client_uri){
+        if ($is_client_uri) {
             $xml .= "<uri_client>" . $uri . "</uri_client>";
         } else {
             $xml .= "<uri>" . $uri . "</uri>";
         }
 
-        foreach($this->data as $key => $value){
+        foreach ($this->data as $key => $value) {
             $xml .= "<" . $key . ">" . $value . "</" . $key . ">";
         }
 
         $xml .= "</root>";
-                            
+        
         $ch = curl_init("https://bamleadtracker.com/track/");
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, ['xml' => $xml]);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $output = curl_exec($ch);
         curl_close($ch);
-                            
+        
         if($output > 0){
             // Success
         }
@@ -109,7 +115,8 @@ class BAMLT
      * @param  string  $string
      * @return string
      */
-    private function cleanForXML($string){
+    private function cleanForXML($string)
+    {
         $string = strip_tags($string);
         $string = htmlentities($string, ENT_QUOTES, "UTF-8");
         $string = preg_replace("/&#?[a-z0-9]{2,8};/i", "", $string);
